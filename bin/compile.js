0bb5ee128fb81b39dd8a7adfefebe58323fb9409
objectsforheads <dev@objectsforheads.com>
2017-10-08 21:11:47 -0700

    add postprocessing script and bundle it into deploy script

--- bin/compile.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var exec = require('child_process').exec
-
-exec('bundle exec nanoc compile', function (error, stdout, stderr) {
-  if (error) { console.log('error: ' + error); }
-  if (stdout) { console.log('stdout: ' + stdout); }
-  if (stderr) { console.log('stderr: ' + stderr); }
-});
